fa3485867df33186dfd714f623f0efddb867fda6
IniEy <135519269+IniEyyy@users.noreply.github.com>
2024-10-21 09:43:31 +0700

    Delete rollingRND directory

--- rollingRND/locationPage/locationPage.js
+++
@@ -1,9 +0,0 @@
-
-const toggleBtn = document.querySelector('.toggle_btn');
-const toggleBtnIcon = document.querySelector('.toggle_btn i')
-const dropdownMenu = document.querySelector('.dropdown_menu');
-toggleBtn.addEventListener('click', () => {
-    dropdownMenu.classList.toggle('show');
-    const isShow = dropdownMenu.classList.contains('show');
-    toggleBtnIcon.classList = isShow?'fa-solid fa-xmark':'fa-solid fa-bars';
-});
